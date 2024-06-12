8f97ae714f806650214c5407d915b02ac438972b
jalal1985khan <jalal1985khan@gmail.com>
2024-06-12 11:36:21 +0530

    Final added naivgation

--- src/modules/SlpRegistration/index.js
+++ src/modules/SlpRegistration/index.js
@@ -205,21 +205,25 @@ function Index() {
     if (!copyAddress) {
       // If toggle switch is turned on, copy communication address to billing address
       setSearchCountry({
-        country: memberSavedInfo ? parseMemSavedInfo.riemembers.country : "",
+        country: memberSavedInfo ? parseMemSavedInfo.riemembers.comcountry : "",
       });
       setSearchCity({
-        city: memberSavedInfo ? parseMemSavedInfo.riemembers.city : "",
+        city: memberSavedInfo ? parseMemSavedInfo.riemembers.comcity : "",
       });
       setSearchState({
-        state: memberSavedInfo ? parseMemSavedInfo.riemembers.state : "",
+        state: memberSavedInfo ? parseMemSavedInfo.riemembers.comstate : "",
       });
       setCommunicationAddress({
-        addressLine1: memberSavedInfo ? parseMemSavedInfo.riemembers.addr1 : "",
-        addressLine2: memberSavedInfo ? parseMemSavedInfo.riemembers.addr2 : "",
+        addressLine1: memberSavedInfo
+          ? parseMemSavedInfo.riemembers.comaddr1
+          : "",
+        addressLine2: memberSavedInfo
+          ? parseMemSavedInfo.riemembers.comaddr2
+          : "",
         country: "",
-        state: memberSavedInfo ? parseMemSavedInfo.riemembers.state : "",
-        city: memberSavedInfo ? parseMemSavedInfo.riemembers.city : "",
-        postalCode: memberSavedInfo ? parseMemSavedInfo.riemembers.pin : "",
+        state: memberSavedInfo ? parseMemSavedInfo.riemembers.comstate : "",
+        city: memberSavedInfo ? parseMemSavedInfo.riemembers.comcity : "",
+        postalCode: memberSavedInfo ? parseMemSavedInfo.riemembers.compin : "",
       });
     } else {
       // If toggle switch is turned off, clear billing address fields
